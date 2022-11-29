7ccf6972a70e099ee6f33cb241aceb8bbfc5b5c5
Dimitar Vezalov <wopopo13@gmail.com>
2022-11-29 22:53:53 +0200

    add main layout

--- restaurant-app/restaurant-ng/app-restaurant/src/app/app-routing.module.ts
+++ restaurant-app/restaurant-ng/app-restaurant/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+    {path: 'contacts', component: ContactsComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- restaurant-app/restaurant-ng/app-restaurant/src/app/app.module.ts
+++ restaurant-app/restaurant-ng/app-restaurant/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
